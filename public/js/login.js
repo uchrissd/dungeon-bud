8e76802e50d0ff56f201dff04119575adf4b687b
Ctucker9233 <court.m.tucker@gmail.com>
2020-01-10 21:22:26 -0800

    Added old paper background to sign in form, added name to the form and to the database, adjusted table associations

--- public/js/login.js
+++ public/js/login.js
@@ -17,6 +17,7 @@ $(document).ready(function() {
     }
 
     // If we have an email and password we run the loginUser function and clear the form
+    // eslint-disable-next-line no-use-before-define
     loginUser(userData.email, userData.password);
     emailInput.val("");
     passwordInput.val("");
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -15,6 +15,7 @@ module.exports = function (app) {
   // otherwise send back an error
   app.post("/api/signup", function (req, res) {
     db.User.create({
+      name: req.body.name,
       email: req.body.email,
       password: req.body.password
     })
@@ -41,6 +42,7 @@ module.exports = function (app) {
       // Otherwise send back the user's email and id
       // Sending back a password, even a hashed password, isn't a good idea
       res.json({
+        name: req.user.name,
         email: req.user.email,
         id: req.user.id
       });
